de1e39557606c5ad4cdba2a2486fe7555c12827b
amirhnajafiz <najafizadeh21@gmail.com>
2022-10-28 21:39:13 +0330

    update: lua load configs

--- pre.lua
+++ pre.lua
@@ -1,7 +1,6 @@
 #!/opt/homebrew/bin/lua
 
-package.path = package.path .. ';local/share/lua/5.3/?.lua'
-inifile = require('inifile')
+inifile = require "inifile"
 
 -- parse the INI file and
 -- put values into a table called conf
@@ -10,7 +9,7 @@ conf = inifile.parse('config.toml')
 -- Clone into github repository for downloading source code into 
 -- src directory.
 
-local github_url = "https://github.com/qemu/qemu"
+local github_url = conf['meta-data']['repository']
 local git_clone_command = "git clone " + github_url
 
 -- Clone into repo.
